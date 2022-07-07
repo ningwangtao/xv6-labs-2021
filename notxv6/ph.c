2a0412073970d3cac402e93e30bb44d18c84d33d
ningwangtao <ningwangtao@gmail.com>
2022-07-07 16:08:35 +0800

    add lab6 thread

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -16,6 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+pthread_mutex_t lock[NBUCKET] = {PTHREAD_MUTEX_INITIALIZER};
 
 
 double
@@ -52,7 +53,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&lock[i]);
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock[i]);
   }
 
 }
